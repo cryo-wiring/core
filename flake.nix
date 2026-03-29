8e2dd914751f0e7e80a0d295c1a2ac9ebf0f9e9a
orangekame3 <orangekame3.dev@gmail.com>
2026-03-29 14:41:14 +0900

    feat: update chip configuration by removing qubits_per_readout_line and refactoring readout line generation

--- flake.nix
+++ flake.nix
@@ -1,80 +1,31 @@
 {
-  description = "cryowire - dilution refrigerator wiring configuration management";
-
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
-  };
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
   outputs =
+    { nixpkgs, ... }:
+    let
+      forEachSystem = nixpkgs.lib.genAttrs [
+        "aarch64-darwin"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "x86_64-linux"
+      ];
+    in
     {
-      nixpkgs,
-      flake-utils,
-      ...
-    }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-        python = pkgs.python313;
-        pythonPkgs = python.pkgs;
-
-        cryowire = pythonPkgs.buildPythonPackage {
-          pname = "cryowire";
-          version = "0.0.0-dev";
-          pyproject = true;
-
-          src = ./.;
-
-          env.SETUPTOOLS_SCM_PRETEND_VERSION = "0.0.0-dev";
-
-          build-system = [
-            pythonPkgs.hatchling
-            pythonPkgs.hatch-vcs
-          ];
-
-          dependencies = [
-            pythonPkgs.jsonschema
-            pythonPkgs.matplotlib
-            pythonPkgs.pydantic
-            pythonPkgs.pyyaml
-            pythonPkgs.rich
-            pythonPkgs.typer
-          ];
-
-          nativeCheckInputs = [
-            pythonPkgs.pytest
-          ];
-
-          checkPhase = ''
-            runHook preCheck
-            pytest tests/ -v
-            runHook postCheck
-          '';
-
-          meta = {
-            description = "JSON Schema validation for cryowire configuration files";
-            homepage = "https://github.com/cryowire/cryowire";
-            license = pkgs.lib.licenses.asl20;
+      devShells = forEachSystem (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          default = pkgs.mkShell {
+            packages = [
+              pkgs.python313
+              pkgs.uv
+            ];
+            env.UV_PYTHON_DOWNLOADS = "never";
           };
-        };
-      in
-      {
-        packages = {
-          default = cryowire;
-          inherit cryowire;
-        };
-
-        devShells.default = pkgs.mkShell {
-          packages = [
-            python
-            pkgs.uv
-          ];
-
-          shellHook = ''
-            echo "cryowire dev shell — Python $(python3 --version | cut -d' ' -f2), uv $(uv --version | cut -d' ' -f2)"
-          '';
-        };
-      }
-    );
+        }
+      );
+    };
 }
